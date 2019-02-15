541e6c9096e01f4c23d11088ebfe1a7caee5343e
gorzhn <gmalevski@gmail.com>
2019-02-16 00:29:50 +0100

    i pray to god everyday

--- resources/assets/js/components/Chat.vue
+++ resources/assets/js/components/Chat.vue
@@ -1,26 +1,82 @@
 <template>
     <div class="container">
-        <h3>Chat</h3>
-      <div id="chatDiv" style='max-height:300px; overflow-y:auto;' v-on:scroll='checkScroll()'>
-      <div v-for="mess in messages.slice().reverse()">
-        <b>{{mess.username}}</b>
-        <p>{{mess.messageBody}}</p>
-        <div :id="mess.message_id">
+      <!-- asd -->
+<div class="messaging">
+  <div class="inbox_msg">
+  <div class="inbox_people">
+    <div class="headind_srch">
+    <div class="recent_heading">
+      <h4>Online </h4>
+    </div>
+    </div>
+    <div class="inbox_chat scroll">
+     <div v-for="userOnline in activeUsers">
+      <div class="chat_list">
+        <div class="chat_people">
+          <div class="chat_img">
+            <img src="https://picsum.photos/200/300"> 
+          </div>
+            <div class="chat_ib">
+                <h5>{{userOnline.name}}</h5> 
+                </div>
         </div>
       </div>
     </div>
-    <div v-if="typingUsers.length != 0">
-     <small v-for="user in typingUsers">
-          {{user.user}}
-      </small>
-      <small v-if="typingUsers.length > 1">are typing...</small>
-      <small v-if="typingUsers.length == 1">is typing...</small>
+    
+
     </div>
-      <input id="inputBox" type="textarea" @input="typing(username)" @focus="hasRead()" v-model="sendMessage" placeholder="Send a message">
-      <button v-on:click="sendAMessage()">Send</button>
-  <div v-for="userOnline in activeUsers">
-      <li>{{userOnline.name}}</li>
   </div>
+  <div class="mesgs">
+    <div  style="height:560px" class="msg_history" id="chatDiv"  v-on:scroll='checkScroll()'>
+
+      
+      <div v-for="mess in messages.slice().reverse()">
+        
+        <div v-if="mess.user_id != user_id">
+         <div class="incoming_msg">
+          <div class="incoming_msg_img"> <img src="https://picsum.photos/g/200/300
+" > </div>
+
+            <div style="-webkit-baseline-middle" class="received_msg">
+              {{mess.username}}
+          <div class="received_withd_msg">
+            <p :title="mess.created_at">{{mess.messageBody}}</p>
+            <span class="time_date"> </span>
+            
+        </div>
+      </div>
+    </div>
+   
+        </div>
+        <div v-else="mess.user_id == user_id">
+          <div class="outgoing_msg">
+      <div class="sent_msg">
+      <p>{{mess.messageBody}}</p>
+       </div>
+    </div>
+    
+        </div>
+        <div :id="mess.message_id">
+        </div>
+      </div>
+    
+
+
+    </div>
+    
+    <div class="type_msg">
+    
+    <div class="input_msg_write">
+     
+      <input   class="write_msg" id="inputBox" type="text" @input="typing(username)" @focus="hasRead()" v-model="sendMessage" placeholder="Send a message">
+
+      <button class="msg_send_btn"  type="button" v-on:click="sendAMessage()"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+
+    </div>
+    </div>
+  </div>
+  </div>
+</div>
   </div>
 </template>
 <script type="text/javascript">
@@ -285,7 +341,7 @@ props:{
             }
         }
       });
-      Echo.private('App.User.' + this.user)
+      Echo.private('App.User.' + this.user_id)
        .notification((notification) => {
        console.log(notification);
        });
@@ -295,5 +351,234 @@ props:{
 
 </script>
 
+
 <style>
+
+.container{
+    max-width:900px;
+}
+.inbox_people {
+  background: #fff;
+  float: left;
+  overflow: hidden;
+  width: 30%;
+  border-right: 1px solid #ddd;
+}
+
+.inbox_msg {
+  border: 1px solid #ddd;
+  clear: both;
+  overflow: hidden;
+}
+
+.top_spac {
+  margin: 20px 0 0;
+}
+
+.recent_heading {
+  float: left;
+  width: 40%;
+}
+
+.srch_bar {
+  display: inline-block;
+  text-align: right;
+  width: 60%;
+  padding:
+}
+
+.headind_srch {
+  padding: 10px 29px 10px 20px;
+  overflow: hidden;
+  border-bottom: 1px solid #c4c4c4;
+}
+
+.recent_heading h4 {
+  color: #0465ac;
+    font-size: 16px;
+    margin: auto;
+    line-height: 29px;
+}
+
+.srch_bar input {
+  outline: none;
+  border: 1px solid #cdcdcd;
+  border-width: 0 0 1px 0;
+  width: 80%;
+  padding: 2px 0 4px 6px;
+  background: none;
+}
+
+.srch_bar .input-group-addon button {
+  background: rgba(0, 0, 0, 0) none repeat scroll 0 0;
+  border: medium none;
+  padding: 0;
+  color: #707070;
+  font-size: 18px;
+}
+
+.srch_bar .input-group-addon {
+  margin: 0 0 0 -27px;
+}
+
+.chat_ib h5 {
+  font-size: 15px;
+  color: #464646;
+  margin: 0 0 8px 0;
+}
+
+.chat_ib h5 span {
+  font-size: 13px;
+  float: right;
+}
+
+.chat_ib p {
+    font-size: 12px;
+    color: #989898;
+    margin: auto;
+    display: inline-block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+.chat_img {
+  float: left;
+  width: 11%;
+}
+
+.chat_img img {
+  width: 100%
+}
+
+.chat_ib {
+  float: left;
+  padding: 0 0 0 15px;
+  width: 88%;
+}
+
+.chat_people {
+  overflow: hidden;
+  clear: both;
+}
+
+.chat_list {
+  border-bottom: 1px solid #ddd;
+  margin: 0;
+  padding: 18px 16px 10px;
+}
+
+.inbox_chat {
+  height: 550px;
+  overflow-y: scroll;
+}
+
+.active_chat {
+  background: #e8f6ff;
+}
+
+.incoming_msg_img {
+  display: inline-block;
+  width: 6%;
+  margin:20px 0 20px 0;
+}
+
+.incoming_msg_img img {
+  width: 100%;
+}
+
+.received_msg {
+  display: inline-block;
+  padding: 0 0 0 10px;
+  vertical-align: top;
+  width: 92%;
+  color:blue;
+  vertical-align:-webkit-baseline-middle;
+}
+
+.received_withd_msg p {
+  background: #ebebeb none repeat scroll 0 0;
+  border-radius: 0 15px 15px 15px;
+  color: #646464;
+  font-size: 14px;
+  margin: 0;
+  padding: 5px 10px 5px 12px;
+  width: 100%;
+}
+
+.time_date {
+  color: #747474;
+  display: block;
+  font-size: 12px;
+  margin: 8px 0 0;
+}
+
+.received_withd_msg {
+  width: 57%;
+}
+
+.mesgs{
+  float: left;
+  padding: 30px 15px 0 25px;
+  width:70%;
+}
+
+.sent_msg p {
+  background:#0465ac;
+  border-radius: 12px 15px 15px 0;
+  font-size: 14px;
+  margin: 0;
+  color: #fff;
+  padding: 5px 10px 5px 12px;
+  width: 100%;
+}
+
+.outgoing_msg {
+  overflow: hidden;
+  margin: 26px 0 26px;
+}
+
+.sent_msg {
+  float: right;
+  width: 46%;
+}
+
+.input_msg_write input {
+  background: rgba(0, 0, 0, 0) none repeat scroll 0 0;
+  border: medium none;
+  color: #4c4c4c;
+  font-size: 15px;
+  min-height: 48px;
+  width: 100%;
+  outline:none;
+}
+
+.type_msg {
+  border-top: 1px solid #c4c4c4;
+  position: relative;
+}
+
+.msg_send_btn {
+  background: #05728f none repeat scroll 0 0;
+  border:none;
+  border-radius: 50%;
+  color: #fff;
+  cursor: pointer;
+  font-size: 15px;
+  height: 33px;
+  position: absolute;
+  right: 0;
+  top: 11px;
+  width: 33px;
+}
+
+.messaging {
+  padding: 0 0 50px 0;
+}
+
+.msg_history {
+
+  overflow-y: scroll;
+}
+
 </style>
